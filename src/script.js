8efab558b15e0bffa9bef29abaa4af445991c44a
Toan <votoan410@gmail.com>
2022-07-31 17:01:36 -0500

    significant changes to script.js to change how the page is rendered

--- src/script.js
+++ src/script.js
@@ -1,32 +1,119 @@
-const ol = document.getElementById('posts');
-// const tbl = document.getElementById('posts');
-// tbl.style.width = '300px';
-// tbl.style.border = '1px solid black';
+// API calls
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const postsPath = 'posts';
 
-
-// create a DOM instance that will hold other elements
-// for the list
-const list = document.createDocumentFragment();
-
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => {
-    let posts = data;
-    console.log(posts);
-
-    posts.map(function (post) {
-      // regular list dom to populate fetched data
-      let li = document.createElement('li');
-      li.innerHTML = ` ${post.title}`;
-      list.appendChild(li);
-
-      // table to fetch data
-      // const tr = tbl.insertRow();
-      // const td = tr.insertCell();
-      // td.style.border = '1px solid black';
-      // td.appendChild(document.createTextNode(`${post.id} ${post.title}`));
-    });
-
-    // append the instance to the default dom
-    ol.appendChild(list);
+// api methods
+const getPosts = () => {
+  const postsEndPoint = [BASE_URL, postsPath].join('/');
+  return fetch(postsEndPoint).then(response => {
+    return response.json();
   });
+};
+
+const deletePost = id => {
+  const postsEndPoint = [BASE_URL, postsPath, id].join('/');
+  return fetch(postsEndPoint, {
+    method: 'DELETE',
+  }).then(response => {
+    return response.json();
+  });
+};
+
+// CONST
+const DomSelectors = {
+  articleContainer: 'article__container',
+  cardProps: {
+    cardArticle: 'card',
+    blueTitle: 'card__tag--blue',
+    greenTitle: 'card__tag--green',
+    cardDate: 'card__date',
+    cardParagraph: 'card__paragraph',
+    cardContinue: 'card__continue',
+    cardHeader: 'card__header',
+  },
+};
+
+// STATE
+class PostState {
+  constructor() {
+    this._posts = [];
+  }
+
+  get posts() {
+    return this._posts;
+  }
+
+  set posts(newPosts) {
+    this._posts = newPosts;
+    renderPostCard(this._posts, document.querySelector('.article__container'));
+  }
+}
+
+let state = new PostState();
+
+// VIEW RENDER function
+const render = (tmp, element) => {
+  element.innerHTML = tmp;
+};
+
+const renderPostCard = (posts, elementToMap) => {
+  // render sth to swhere
+  // in this case is to the card section
+  const tmp =
+    posts !== undefined
+      ? posts
+          .map(post => generatePostCardTmp(post, DomSelectors.cardProps))
+          .join('')
+      : [];
+
+  elementToMap !== undefined ? render(tmp, elementToMap) : null;
+};
+
+// Template
+const generatePostCardTmp = (
+  post,
+  { cardArticle, blueTitle, cardDate, cardParagraph, cardContinue, cardHeader }
+) => {
+  return `<article class="${cardArticle}">
+            <header>
+              <span class="${blueTitle}">${post.id}</span>
+              <h2 class="${cardHeader}">${post.title}</h2>
+              <span class="${cardDate}">Nov 12</span>
+            </header>
+            <p class="${cardParagraph}">
+              ${post.body}
+            </p>
+          <button class="btn-delete" name="btn-delete-${post.id}"  >Delete this post</button>
+        </article>`;
+};
+
+const setUpCardsEvent = cardsElement => {
+  cardsElement.addEventListener('click', event => {
+    console.log(event.target);
+    if (event?.target?.name?.startsWith('btn-delete')) {
+      const id = +event.target.name.substring(11);
+      console.log(id);
+      deletePost(id).then(data => {
+        state.posts = state.posts.filter(post => {
+          return post.id !== id;
+        });
+      });
+    }
+  });
+};
+
+// INIT
+const init = () => {
+  renderPostCard();
+  const cardsElement =  document.querySelector(
+    ".article__container"
+  );
+  console.log('card element val: ' + cardsElement);
+  setUpCardsEvent(cardsElement);
+
+  getPosts().then(data => {
+    state.posts = data;
+  });
+};
+
+init();
